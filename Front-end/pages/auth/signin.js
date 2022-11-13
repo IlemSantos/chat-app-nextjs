863ee2416cdfad06b947ac8f3fbd0e8b30ee975f
Ilem Santos <ilemlima@gmail.com>
2022-11-13 19:16:15 -0400

    first commit signin signup

--- Front-end/pages/auth/signin.js
+++ Front-end/pages/auth/signin.js
@@ -26,46 +26,43 @@ export default function SignIn() {
   }
 
   return (
-    <>
-      <Flex height="100vh" alignItems="center" justifyContent="center">
-        <Flex direction="column" background="gray.100" p={12} rounded={6}>
-          <Heading mb={6}>Log in</Heading>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background="gray.100" minW={["full", "md"]} p={12} rounded={6}>
+        <Heading mb={6}>Log in</Heading>
 
-          <form onSubmit={handleSubmit(handleSignIn)}>
+        <form onSubmit={handleSubmit(handleSignIn)}>
+          <FormControl isInvalid={errors.email} mb={2}>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input {...register('email', {
+              required: 'This is required',
+            })} id="email" name="email" type="email" />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
 
-            <FormControl isInvalid={errors.email} mb={2}>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <Input {...register('email', {
-                required: 'This is required',
-              })} id="email" name="email" type="email" />
-              <FormErrorMessage>
-                {errors.email && errors.email.message}
-              </FormErrorMessage>
-            </FormControl>
+          <FormControl isInvalid={errors.password} mb={2}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input {...register('password', {
+              required: 'This is required',
+              minLength: { value: 4, message: 'Minimum length should be 4' },
+            })} id="password" name="password" type="password" />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+          </FormControl>
 
-            <FormControl isInvalid={errors.password} mb={2}>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <Input {...register('password', {
-                required: 'This is required',
-                minLength: { value: 4, message: 'Minimum length should be 4' },
-              })} id="password" name="password" type="password" />
-              <FormErrorMessage>
-                {errors.password && errors.password.message}
-              </FormErrorMessage>
-            </FormControl>
+          <Link href="#"><a><Text fontSize="sm">Forgot your password?</Text></a></Link>
+          <Button width="100%" mt={4} borderRadius="100vh" colorScheme="teal" type="submit">Sign in</Button>
+        </form>
 
-            <Link href={"#"}><a><Text fontSize="sm">Forgot your password?</Text></a></Link>
-            <Button width="100%" mt={4} borderRadius={"100vh"} colorScheme="teal" type="submit">Sign in</Button>
-          </form>
+        <Divider my={4} />
 
-          <Divider my={4} />
-
-          <Center>Don't have an account?</Center>
-          <Link href={"/auth/signup"}>
-            <Button width="100%" mt={4} borderRadius={"100vh"} colorScheme="teal" variant='outline'>Sign up</Button>
-          </Link>
-        </Flex>
+        <Center>Don't have an account?</Center>
+        <Link href="/auth/signup">
+          <Button width="100%" mt={4} borderRadius="100vh" colorScheme="teal" variant='outline'>Sign up</Button>
+        </Link>
       </Flex>
-    </>
+    </Flex>
   )
 }
--- Front-end/pages/auth/signup.js
+++ Front-end/pages/auth/signup.js
@@ -1,15 +1,13 @@
-import { useRouter } from "next/router"
+import { useContext } from "react"
 import { Button, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input, Text, useToast } from "@chakra-ui/react"
 import { useForm } from "react-hook-form"
 import Link from "next/link"
-import { useContext } from "react"
 import { AuthContext } from "../../contexts/AuthContext"
 
 export default function SignUp() {
   const { signUp } = useContext(AuthContext)
   const { handleSubmit, register, formState: { errors } } = useForm()
   const toast = useToast()
-  const router = useRouter();
 
   async function handleSignUp(data) {
     const res = await signUp({
@@ -28,38 +26,36 @@ export default function SignUp() {
   }
 
   return (
-    <>
-      <Flex height="100vh" alignItems="center" justifyContent="center">
-        <Flex direction="column" background="gray.100" p={12} rounded={6}>
-          <Heading mb={6}>Register</Heading>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background="gray.100" minW={["full", "md"]} p={12} rounded={6}>
+        <Heading mb={6}>Register</Heading>
 
-          <form onSubmit={handleSubmit(handleSignUp)}>
-            <FormControl isInvalid={errors.email} mb={2}>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <Input {...register('email', {
-                required: 'This is required',
-              })} id="email" name="email" type="email" />
-              <FormErrorMessage>
-                {errors.email && errors.email.message}
-              </FormErrorMessage>
-            </FormControl>
+        <form onSubmit={handleSubmit(handleSignUp)}>
+          <FormControl isInvalid={errors.email} mb={2}>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input {...register('email', {
+              required: 'This is required',
+            })} id="email" name="email" type="email" />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
 
-            <FormControl isInvalid={errors.password} mb={2}>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <Input {...register('password', {
-                required: 'This is required',
-                minLength: { value: 4, message: 'Minimum length should be 4' },
-              })} id="password" name="password" type="password" />
-              <FormErrorMessage>
-                {errors.password && errors.password.message}
-              </FormErrorMessage>
-            </FormControl>
+          <FormControl isInvalid={errors.password} mb={2}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input {...register('password', {
+              required: 'This is required',
+              minLength: { value: 4, message: 'Minimum length should be 4' },
+            })} id="password" name="password" type="password" />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+          </FormControl>
 
-            <Button width="100%" mt={4} borderRadius={"100vh"} colorScheme="teal" type="submit">Sign up</Button>
-          </form>
-          <Text textAlign={['center', 'right']} mt={4} fontSize="sm">Already registered <Link href={"/auth/signin"}><a><Text fontSize="sm">sign in?</Text></a></Link></Text>
-        </Flex>
+          <Button width="100%" mt={4} borderRadius="100vh" colorScheme="teal" type="submit">Sign up</Button>
+        </form>
+        <Text textAlign={['center', 'right']} mt={6} fontSize="sm">Already registered <Link href="/auth/signin"><a>sign in?</a></Link></Text>
       </Flex>
-    </>
+    </Flex>
   )
 }
