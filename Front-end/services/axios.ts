b26d19f2f0195022939a0f0dc5b676f468aec4a4
Ilem Santos <ilemlima@gmail.com>
2022-11-14 09:37:46 -0400

    Services axios

--- Front-end/services/axios.ts
+++ Front-end/services/axios.ts
@@ -5,14 +5,20 @@ export function getAPIClient(ctx?: any) {
   const { 'nextauth.access_token': access_token } = parseCookies(ctx)
 
   const api = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'http://localhost:4000',
+    timeout: 1000,
   })
 
-  api.interceptors.request.use(config => {
-    console.log(config);
+  // Adiciona um interceptador na requisição
+  api.interceptors.request.use(function (config) {
+    // console.log(config);
 
+    // Faz alguma coisa antes da requisição ser enviada
     return config;
-  })
+  }, function (error) {
+    // Faz alguma coisa com o erro da requisição
+    return Promise.reject(error);
+  });
 
   if (access_token) {
     api.defaults.headers['Authorization'] = `Bearer ${access_token}`;
